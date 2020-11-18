5b4814bac283ec5370a54666351b9dc08cb5791e
elvinciqueira <elvinciqueira@gmail.com>
2020-11-18 17:45:44 -0300

    assertion library

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,21 +1,16 @@
-const { sum, subtract } = require('../math')
+const sum = (a, b) => a + b
+const subtract = (a, b) => a - b
 
-let result, expected
+let result = sum(3, 7)
+let expected = 10
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
 
 result = subtract(7, 3)
 expected = 4
-expect(result).toBe(expected)
 
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
-    }
-  }
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
 }
