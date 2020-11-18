8b42faec9f38e897e4eac72c4157d0ec3d4a0f1d
elvinciqueira <elvinciqueira@gmail.com>
2020-11-18 17:42:56 -0300

    Abstract test assertions into a Javascript assertion library

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,14 +1,21 @@
-const sum = (a, b) => a + b
-const subtract = (a, b) => a - b
+const { sum, subtract } = require('../math')
 
-let result = sum(3, 7)
-let expected = 10
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
-}
+let result, expected
+
+result = sum(3, 7)
+expected = 10
+expect(result).toBe(expected)
 
 result = subtract(7, 3)
 expected = 4
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+expect(result).toBe(expected)
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`)
+      }
+    }
+  }
 }
